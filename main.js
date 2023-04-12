ac34abcb349442e4bc398c78ce6e500020b97ab2
jacobrees <jacob.rees97@yahoo.com>
2023-04-12 05:57:10 +0100

    Delete all expertise content replace with about me content

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 import Typed from "typed.js";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -22,38 +22,12 @@ gsap.from(".my-expertise-title", {
   ease: "power4.out",
 });
 
-gsap.from(".my-expertise-1", {
+gsap.to(".about-me-title", {
   scrollTrigger: {
-    trigger: ".my-expertise-1",
+    trigger: ".about-me-title",
     start: "top bottom",
   },
-  y: 50,
-  opacity: 0,
-  delay: 0.2,
+  x: 150,
+  
   duration: 1,
-  ease: "power4.out",
-})
-
-gsap.from(".my-expertise-2", {
-  scrollTrigger: {
-    trigger: ".my-expertise-2",
-    start: "top bottom",
-  },
-  y: 50,
-  delay: 0.4,
-  opacity: 0,
-  duration: 1,
-  ease: "power4.out",
-})
-
-gsap.from(".my-expertise-3", {
-  scrollTrigger: {
-    trigger: ".my-expertise-3",
-    start: "top bottom",
-  },
-  y: 50,
-  delay: 0.6,
-  opacity: 0,
-  duration: 1,
-  ease: "power4.out",
 })
