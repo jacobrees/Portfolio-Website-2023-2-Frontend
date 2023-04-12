6f14941e4c841b3154ceb9ec99d1a11d733920f3
jacobrees <jacob.rees97@yahoo.com>
2023-04-12 03:53:25 +0100

    Add gsap to project

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-import './style.css'
-import Typed from 'typed.js';
+import "./style.css";
+import Typed from "typed.js";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const typed = new Typed('.dynamic-description-text', {
-  strings: ['Frontend', 'Backend', 'Full Stack', 'Creative'],
+gsap.registerPlugin(ScrollTrigger);
+
+new Typed(".dynamic-description-text", {
+  strings: ["Frontend", "Backend", "Full Stack", "Creative"],
   typeSpeed: 100,
   loop: true,
-})
+});
+
+gsap.from(".my-expertise-title", {
+  scrollTrigger: '.my-expertise-title',
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power4.out",
+});
