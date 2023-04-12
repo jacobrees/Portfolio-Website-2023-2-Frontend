4fcff7c4c401493b2d638d756eb030e9d5a75324
jacobrees <jacob.rees97@yahoo.com>
2023-04-12 05:13:21 +0100

    Complete expertise section

--- main.js
+++ main.js
@@ -12,9 +12,48 @@ new Typed(".dynamic-description-text", {
 });
 
 gsap.from(".my-expertise-title", {
-  scrollTrigger: '.my-expertise-title',
+  scrollTrigger: {
+    trigger: ".my-expertise-title",
+    start: "top bottom",
+  },
   y: 50,
   opacity: 0,
   duration: 1,
   ease: "power4.out",
 });
+
+gsap.from(".my-expertise-1", {
+  scrollTrigger: {
+    trigger: ".my-expertise-1",
+    start: "top bottom",
+  },
+  y: 50,
+  opacity: 0,
+  delay: 0.2,
+  duration: 1,
+  ease: "power4.out",
+})
+
+gsap.from(".my-expertise-2", {
+  scrollTrigger: {
+    trigger: ".my-expertise-2",
+    start: "top bottom",
+  },
+  y: 50,
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  ease: "power4.out",
+})
+
+gsap.from(".my-expertise-3", {
+  scrollTrigger: {
+    trigger: ".my-expertise-3",
+    start: "top bottom",
+  },
+  y: 50,
+  delay: 0.6,
+  opacity: 0,
+  duration: 1,
+  ease: "power4.out",
+})
