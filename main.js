3210c53faccbb0f50c0a870edfb37a649959f246
jacobrees <jacob.rees97@yahoo.com>
2023-04-12 02:21:50 +0100

    Add typed.js into project

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import './style.css'
+import Typed from 'typed.js';
 
+const typed = new Typed('.dynamic-description-text', {
+  strings: ['Frontend', 'Backend', 'Full Stack', 'Creative'],
+  typeSpeed: 100,
+  loop: true,
+})
