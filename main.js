94685fa6bcdb533ea9904296b561d5a2c240873e
jacobrees <jacob.rees97@yahoo.com>
2023-09-30 22:04:38 +0100

    Add full stack to the dynamic text on home screen

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ closeNavButton.addEventListener("click", () => {
 gsap.registerPlugin(ScrollTrigger);
 
 new Typed(".dynamic-description-text", {
-  strings: ["Frontend", "Creative"],
+  strings: ["Frontend", "Creative", "Full Stack"],
   typeSpeed: 100,
   loop: true,
 });
